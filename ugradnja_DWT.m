41a4a58c2a22f17e9219c3e01eacc98cbe55f14e
Nikola Jovanovic <etfovac@yahoo.com>
2020-08-12 23:35:28 +0200

    Update ugradnja_DWT.m
    #5

--- ugradnja_DWT.m
+++ ugradnja_DWT.m
@@ -1,33 +1,33 @@
-function Zig_slika = ugradnja_DWT(slika, skrembl_zig1)
+function Marked_image = ugradnja_DWT(Image, skrembl_zig1)
 % Generisanje slike oznacene koriscenjem Wavelet transformacije-----------
-global Nivo K
-% Nivo = 3
+global Level K
+% Level = 3
 %-------------------------------------------------------------------------
-[DWT_slike, knjigovodja] = wavedec2(slika, Nivo, 'haar');
-Zig_DWT_slike = DWT_slike;  % inicijalizacija
+[Image_DWT, bookie] = wavedec2(Image, Level, 'haar');
+Marked_Image_DWT = Image_DWT;  % init
 %-------------------------------------------------------------------------
-horiz_detalji3 = wavecopy('h', DWT_slike, knjigovodja, Nivo);
-vert_detalji3 = wavecopy('v', DWT_slike, knjigovodja, Nivo);
-dijag_detalji3 = wavecopy('d', DWT_slike, knjigovodja, Nivo);
+horiz_detalji3 = wavecopy('h', Image_DWT, bookie, Level);
+vert_detalji3 = wavecopy('v', Image_DWT, bookie, Level);
+dijag_detalji3 = wavecopy('d', Image_DWT, bookie, Level);
 zig_horiz_detalji3 = horiz_detalji3 + K/2/100 * abs(horiz_detalji3).*skrembl_zig1;
 zig_vert_detalji3 = vert_detalji3 + K/2/100 * abs(vert_detalji3) .* skrembl_zig1;
 zig_dijag_detalji3 = dijag_detalji3 + K/2/100 * abs(dijag_detalji3) .* skrembl_zig1;
 %-------------------------------------------------------------------------
-Zig_DWT_slike = wavepaste('h', Zig_DWT_slike, knjigovodja, Nivo, zig_horiz_detalji3);
-Zig_DWT_slike = wavepaste('v', Zig_DWT_slike, knjigovodja, Nivo, zig_vert_detalji3);
-Zig_DWT_slike = wavepaste('d', Zig_DWT_slike, knjigovodja, Nivo, zig_dijag_detalji3);
+Marked_Image_DWT = wavepaste('h', Marked_Image_DWT, bookie, Level, zig_horiz_detalji3);
+Marked_Image_DWT = wavepaste('v', Marked_Image_DWT, bookie, Level, zig_vert_detalji3);
+Marked_Image_DWT = wavepaste('d', Marked_Image_DWT, bookie, Level, zig_dijag_detalji3);
 %-------------------------------------------------------------------------
-horiz_detalji2 = wavecopy('h', DWT_slike, knjigovodja, Nivo-1);
-vert_detalji2 = wavecopy('v', DWT_slike, knjigovodja, Nivo-1);
-dijag_detalji2 = wavecopy('d', DWT_slike, knjigovodja, Nivo-1);
-skrembl_zig2 = repmat(skrembl_zig1, Nivo - 1);
-% skrembl_zig2 = [skrembl_zig1, skrembl_zig1; skrembl_zig1, skrembl_zig1];
-zig_horiz_detalji2 = horiz_detalji2 + 2*K/100 * abs(horiz_detalji2).*skrembl_zig2;
-zig_vert_detalji2 = vert_detalji2 + 2*K/100 * abs(vert_detalji2) .* skrembl_zig2;
+hor_detailes_2 = wavecopy('h', Image_DWT, bookie, Level-1);
+ver_detailes_2 = wavecopy('v', Image_DWT, bookie, Level-1);
+dijag_detalji2 = wavecopy('d', Image_DWT, bookie, Level-1);
+skrembl_zig2 = repmat(skrembl_zig1, Level - 1);
+
+mkd_hor_detailes_2 = hor_detailes_2 + 2*K/100 * abs(hor_detailes_2).*skrembl_zig2;
+mkd_ver_detailes_2 = ver_detailes_2 + 2*K/100 * abs(ver_detailes_2) .* skrembl_zig2;
 zig_dijag_detalji2 = dijag_detalji2 + 2*K/100 * abs(dijag_detalji2) .* skrembl_zig2;
 %-------------------------------------------------------------------------
-Zig_DWT_slike = wavepaste('h', Zig_DWT_slike, knjigovodja, Nivo-1, zig_horiz_detalji2);
-Zig_DWT_slike = wavepaste('v', Zig_DWT_slike, knjigovodja, Nivo-1, zig_vert_detalji2);
-Zig_DWT_slike = wavepaste('d', Zig_DWT_slike, knjigovodja, Nivo-1, zig_dijag_detalji2);
+Marked_Image_DWT = wavepaste('h', Marked_Image_DWT, bookie, Level-1, mkd_hor_detailes_2);
+Marked_Image_DWT = wavepaste('v', Marked_Image_DWT, bookie, Level-1, mkd_ver_detailes_2);
+Marked_Image_DWT = wavepaste('d', Marked_Image_DWT, bookie, Level-1, zig_dijag_detalji2);
 %-------------------------------------------------------------------------
-Zig_slika = waverec2(Zig_DWT_slike, knjigovodja, 'haar');
+Marked_image = waverec2(Marked_Image_DWT, bookie, 'haar');
